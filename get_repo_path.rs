acb922140a9116a1823f0bc5b71ad525ae9008ce
Nicholas Bishop <nicholasbishop@gmail.com>
2021-08-28 16:08:41 -0400

    Update get_repo_path

--- get_repo_path.rs
+++ get_repo_path.rs
@@ -1,7 +1,27 @@
 // Deps:
+// anyhow = "1.0"
 // camino = "1.0"
+// fehler = "1.0"
 
+use anyhow::{anyhow, Error};
 use camino::Utf8PathBuf;
+use fehler::throws;
+use std::env;
+
+/// Get the absolute path of the repo. Assumes that this executable is
+/// located at <repo>/target/<buildmode>/<exename>.
+#[throws]
+fn get_repo_path() -> Utf8PathBuf {
+    let exe = Utf8PathBuf::from_path_buf(env::current_exe()?)
+        .map_err(|_| anyhow!("exe path is not utf-8"))?;
+    exe.parent()
+        .map(|path| path.parent())
+        .flatten()
+        .ok_or_else(|| anyhow!("not enough parents: {}", exe))?
+        .into()
+}
+
+// Option version:
 
 /// Get the absolute path of the repo. Assumes that this executable is
 /// located at <repo>/target/<buildmode>/<exename>.
